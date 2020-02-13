b9d0f6f6a03335c2d4bed03e9b2e97faf9194a22
Maxwell Lu <blu@lcs.on.ca>
2020-02-12 19:18:55 -0500

    Initial Commit

---
+++ War/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  War
+//
+//  Created by Maxwell Lu on 2020-02-12.
+//  Copyright © 2020 Maxwell Lu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
